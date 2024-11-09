ef78f2acf48d2cfc311d09e8b74fdb692f583865
= <=>
2024-11-10 03:54:30 +0530

    3st Commit

--- conn/conn.js
+++ conn/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const conn = async () => {
     try {
-        const response = await mongoose.connect(`${process.env.MONGO_URI}`);
+        const response = await mongoose.connect("mongodb+srv://ayesh3121:ayesh3121@cluster0.jhqca.mongodb.net/TaskManagement");
         if(response){
             console.log("connected to DB")
         }
